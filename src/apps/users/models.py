78e55783702a48302cd7f5613ea83a03e378ecc2
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-05-19 18:26:03 +0500

    codes

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -4,8 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True, null=True)
-
-    def __str__(self):
-        return self.username
-
+    pass
